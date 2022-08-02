39518c2382450b3010d5acee24522bf1a3a417ec
Aristeidis Bampakos <bampakoa@gmail.com>
2022-08-02 10:37:48 +0300

    Remove product-list component from Chapter 3

--- ch03/src/app/products/products.module.ts
+++ ch03/src/app/products/products.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductListComponent } from './product-list/product-list.component';
 
 
 
 @NgModule({
-  declarations: [
-    ProductListComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule
-  ],
-  exports: [ProductListComponent]
+  ]
 })
 export class ProductsModule { }
