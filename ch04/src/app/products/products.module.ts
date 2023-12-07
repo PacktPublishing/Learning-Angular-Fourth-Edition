39a0225227a534a9df927cc13066e56d64059343
Kenneth Barrett <hamptonalumkb@proton.me>
2023-12-07 18:44:54 -0500

    Created custom sort pipe and use on products list

--- ch04/src/app/products/products.module.ts
+++ ch04/src/app/products/products.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    SortPipe
   ],
   imports: [
     CommonModule
